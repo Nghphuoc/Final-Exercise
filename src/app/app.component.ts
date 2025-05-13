e1713659e94f73b3ff7a2f1e1130b52ecd28b7b9
data Test <phuocvk9000@gmail.com>
2025-05-13 10:48:20 +0700

    modify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigateComponentComponent } from "./navigate/navigate-component/navigate-component.component";
-import { UserService } from './user-component/userService/user.service';
 
 @Component({
   selector: 'app-root',
